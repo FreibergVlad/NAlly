7dcb580cdd6df3b2a92c2441a402eb635358d798
vfreiberg <>
2020-06-09 14:52:35 +0300

    Sniffer improvements
    * Rework Sniffer to yield received packets
    * Support started_callback in Sniffer
    * Removed AsyncSniffer

--- nally/core/sniffer/sniffer.py
+++ nally/core/sniffer/sniffer.py
@@ -2,12 +2,14 @@ import logging
 import socket
 import selectors
 import time
+from typing import Generator
 
 from pcapy import BPFProgram
 
 from nally.config import config
 from nally.core.layers.link.ethernet.ethernet_packet import EthernetPacket
 from nally.core.layers.link.ethernet.ethernet_utils import EthernetUtils
+from nally.core.layers.packet import Packet
 from nally.core.utils.platform_specific.platform_specific_utils import PlatformSpecificUtils
 
 
@@ -23,8 +25,8 @@ class Sniffer:
 
     def __init__(
             self,
-            callback: callable,
             if_name: str = None,
+            started_callback: callable = None,
             packet_count: int = None,
             promiscuous_mode: bool = True,
             bpf_filter: str = "",
@@ -33,7 +35,7 @@ class Sniffer:
         """
         :param if_name: network interface for capturing, if not specified,
             then sniffer will try to pick up the default one
-        :param callback: function which will be called when the new packet caught
+        :param started_callback: function which will be called after the sniffer initialization
         :param packet_count: number of packets that should be caught
         :param promiscuous_mode: indicates if sniffer should receive all packets on the LAN, including packets sent to
             a network address that the network adapter isn't configured to recognize.
@@ -41,8 +43,8 @@ class Sniffer:
         :param timeout: specifies timeout in seconds after which sniffer will be terminated
         """
         self._if_name = if_name if if_name is not None else config.interface_name
+        self._started_callback = started_callback
         self._packet_count = packet_count
-        self._callback = callback
         self._promiscuous_mode = promiscuous_mode
         self._bpf_filter = bpf_filter
         self._timeout = timeout
@@ -50,16 +52,20 @@ class Sniffer:
         self._sniff_socket = None
         self._compiled_filter = None
 
-    def sniff(self) -> int:
+    def sniff(self) -> Generator[Packet, None, int]:
         processed_count = 0
         try:
             if self._sniff_socket is None:
                 raise RuntimeError('Sniffer should be used inside context manager')
             termination_date_seconds = None
+            if self._started_callback is not None:
+                self._started_callback()
             if self._timeout is not None:
                 termination_date_seconds = time.time() + self._timeout
             remaining_time_seconds = None
             while not self._stopped:
+                if processed_count == self._packet_count:
+                    break
                 if self._timeout is not None:
                     remaining_time_seconds = termination_date_seconds - time.time()
                     if remaining_time_seconds <= 0:
@@ -71,39 +77,25 @@ class Sniffer:
                     continue
                 # data is available
                 packet_addr: tuple = self._sniff_socket.recvfrom(self.BUFFER_SIZE_BYTES)
-                packet: bytes = packet_addr[0]
-                if self._process_packet(packet):
+                raw_packet: bytes = packet_addr[0]
+                if self._filter_packet(raw_packet):
+                    # parse Ethernet header and all upper layers if present
+                    ethernet_packet = EthernetPacket.from_bytes(raw_packet)
                     processed_count += 1
-                if processed_count == self._packet_count:
-                    break
+                    yield ethernet_packet
             return processed_count
         except KeyboardInterrupt:
-            self.LOG.info("Keyboard interruption received. Exiting...")
+            self.LOG.debug("Keyboard interruption received. Exiting...")
             return processed_count
 
     def stop(self):
         """
         Provides ability to gracefully terminate the sniffer (makes sense if sniffer is running in
-        the separate thread, see AsyncSniffer class)
+        the separate thread)
         """
         if self._stopped:
             raise RuntimeError("Illegal state: sniffer already terminated")
         self._stopped = True
-
-    def _process_packet(self, raw_packet: bytes) -> bool:
-        """
-        Processes arrived raw packet, applies necessary filters and passes
-        it to the user-defined callback
-
-        :param raw_packet: raw network packet
-        :return: True, if packet was processed successfully, False otherwise
-        """
-        if self._filter_packet(raw_packet):
-            # parse Ethernet header and all upper layers if present
-            ethernet_packet = EthernetPacket.from_bytes(raw_packet)
-            self._callback(ethernet_packet)
-            return True
-        return False
 
     def _compile_filter(self):
         """
@@ -143,6 +135,14 @@ class Sniffer:
         self._compile_filter()
         self._selector = selectors.DefaultSelector()
         self._selector.register(self._sniff_socket, selectors.EVENT_READ)
+        self.LOG.debug(
+            "Sniffer had been initialized with the following options: "
+            f"promiscuous_mode={self._promiscuous_mode}, "
+            f"bpf_filter='{self._bpf_filter}', "
+            f"timeout={self._timeout}, "
+            f"if_name={self._if_name}, "
+            f"packet_count={self._packet_count}"
+        )
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
